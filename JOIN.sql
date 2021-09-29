e02cb62d98ae7281fa976216db6fd4e3a07dcfdf
Riccarpa <riccardopagni99@gmail.com>
2021-09-29 15:42:12 +0200

    snack 2

--- JOIN.sql
+++ JOIN.sql
@@ -4,3 +4,10 @@ SELECT `students`.*, `degrees`.`name`
 FROM `students`
 JOIN `degrees` ON `degree_id` = `students`.`id`
 WHERE `degrees`.`name` = 'corso di laurea in economia';
+
+-- 2. Selezionare tutti i Corsi di Laurea del Dipartimento di Neuroscienze
+SELECT `degrees`.`name`,`departments`.`name` 
+FROM `degrees`
+JOIN `departments` ON `department_id` = `departments`.`id`
+WHERE departments.name  = 'dipartimento di neuroscienze';
+
